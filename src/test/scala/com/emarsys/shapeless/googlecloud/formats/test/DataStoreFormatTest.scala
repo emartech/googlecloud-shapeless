9a87d368102e25ce3129c045e3469eb378129aeb
p3a <andras.papp@emarsys.com>
2016-11-27 21:05:01 +0100

    [datastore] refactor tests to wordspec

--- src/test/scala/com/emarsys/shapeless/googlecloud/formats/test/DataStoreFormatTest.scala
+++ src/test/scala/com/emarsys/shapeless/googlecloud/formats/test/DataStoreFormatTest.scala
@@ -8,95 +8,108 @@ import org.joda.time.{DateTime, DateTimeZone}
 import org.scalatest._
 import shapeless._
 
-class DataStoreFormatTest extends FlatSpec with Matchers {
+class DataStoreFormatTest extends WordSpec with Matchers {
 
   import TestTypes._
 
-  "Entity" should "be built from HNil" in {
-    val event: HNil = HNil
-    event.toEntity("test", "test", List()) shouldBe Entity.newBuilder().build()
-  }
+  "DataSore Entity" when {
 
-  "Entity" should "be built from the simplest case class" in {
-    val event = TestInt(1)
-    event.toEntity("test", "test", List()).getProperties.get("value").getIntegerValue shouldBe 1
-  }
+    "created from a case class" should {
+      "be built from HNil" in {
+        val event: HNil = HNil
+        event.toEntity("test", "test", List()) shouldBe Entity.newBuilder().build()
+      }
+      "be built from the simplest case class" in {
+        val event = TestInt(1)
+        event.toEntity("test", "test", List()).getProperties.get("value").getIntegerValue shouldBe 1
+      }
 
-  "Entity" should "be built from the simple case class with string and int" in {
-    val event = TestStringInt("string", 1)
-    val entity = event.toEntity("test", "test", List())
-    entity.getProperties.get("i").getIntegerValue shouldBe 1
-    entity.getProperties.get("s").getStringValue shouldBe "string"
-  }
+      "be built from the simple case class with string and int" in {
+        val event = TestStringInt("string", 1)
+        val entity = event.toEntity("test", "test", List())
+        entity.getProperties.get("i").getIntegerValue shouldBe 1
+        entity.getProperties.get("s").getStringValue shouldBe "string"
+      }
 
-  "Entity" should "be built from simple case class with double value" in {
-    val event = TestStringIntDouble("simple", 1, 0.456789)
-    val entity = event.toEntity("test", "test", List())
-    entity.getProperties.get("i").getIntegerValue shouldBe 1
-    entity.getProperties.get("s").getStringValue shouldBe "simple"
-    entity.getProperties.get("d").getDoubleValue shouldBe 0.456789
-  }
+      "be built from simple case class with double value" in {
+        val event = TestStringIntDouble("simple", 1, 0.456789)
+        val entity = event.toEntity("test", "test", List())
+        entity.getProperties.get("i").getIntegerValue shouldBe 1
+        entity.getProperties.get("s").getStringValue shouldBe "simple"
+        entity.getProperties.get("d").getDoubleValue shouldBe 0.456789
+      }
 
-  "Entity" should "be built from case class with boolean value" in {
-    val trueEvent = TestBoolean(true)
-    val falseEvent = TestBoolean(false)
-    val trueEntity = trueEvent.toEntity("test", "test", List())
-    val falseEntity = falseEvent.toEntity("test","test", List())
-    trueEntity.getProperties.get("condition").getBooleanValue shouldBe true
-    falseEntity.getProperties.get("condition").getBooleanValue shouldBe false
-  }
+      "be built from case class with boolean value" in {
+        val trueEvent = TestBoolean(true)
+        val falseEvent = TestBoolean(false)
+        val trueEntity = trueEvent.toEntity("test", "test", List())
+        val falseEntity = falseEvent.toEntity("test", "test", List())
+        trueEntity.getProperties.get("condition").getBooleanValue shouldBe true
+        falseEntity.getProperties.get("condition").getBooleanValue shouldBe false
+      }
 
-  "Entity" should "be built from case class with joda date value" in {
-    val date: DateTime = new DateTime(2016, 11, 12, 13, 14, 50, DateTimeZone.UTC)
-    val event = TestDateTime(date)
-    val entity: Entity = event.toEntity("test", "test", List())
-    val timestampPb = entity.getProperties.get("date").getTimestampValue
-    val milliSeconds: Long = timestampPb.getSeconds * 1000
-    entity.getProperties.get("date").parseAs[DateTime] shouldBe date
-  }
+      "be built from case class with joda date value" in {
+        val date: DateTime = new DateTime(2016, 11, 12, 13, 14, 50, DateTimeZone.UTC)
+        val event = TestDateTime(date)
+        val entity: Entity = event.toEntity("test", "test", List())
+        val timestampPb = entity.getProperties.get("date").getTimestampValue
+        val milliSeconds: Long = timestampPb.getSeconds * 1000
+        entity.getProperties.get("date").parseAs[DateTime] shouldBe date
+      }
+    }
 
+    "parsed to a case class" should {
 
-  "Entity" should "be parsable to HNil" in {
-    val entity = Entity.newBuilder().build()
-    entity.parseTo[HNil] shouldBe HNil
-  }
+      "be parsable to HNil" in {
+        val entity = Entity.newBuilder().build()
+        entity.parseTo[HNil] shouldBe HNil
+      }
 
-  "Entity" should "be parsable to Simple case class" in {
-    val entityBuilder = Entity.newBuilder()
-    entityBuilder.getMutableProperties.put("value", DatastoreHelper.makeValue(1).build())
-    val entity: Entity = entityBuilder.build()
-    entity.parseTo[TestInt] shouldBe TestInt(1)
-  }
+      "be parsable to Simple case class" in {
+        val entityBuilder = Entity.newBuilder()
+        entityBuilder.getMutableProperties.put("value", DatastoreHelper.makeValue(1).build())
+        val entity: Entity = entityBuilder.build()
+        entity.parseTo[TestInt] shouldBe TestInt(1)
+      }
+    }
 
-  "Simple case class with different fields" should "roundtrip" in {
-    val event = TestStringIntDouble("simple", 1, 0.456789)
-    val entity = event.toEntity("test", "test", List())
-    entity.parseTo[TestStringIntDouble] shouldBe event
-  }
+    "created and then parsed to a case class" should {
 
-  "case class with optional values" should "roundtrip" in {
-    val event = TestOptional(None, Some("some"), None)
-    val entity = event.toEntity("test", "test", List())
-    entity.parseTo[TestOptional] shouldBe event
-  }
+      "work with different field types" in {
+        val event = TestStringIntDouble("simple", 1, 0.456789)
+        val entity = event.toEntity("test", "test", List())
+        entity.parseTo[TestStringIntDouble] shouldBe event
+      }
 
-  "Entity" should "have uuid key if no key field is defined" in {
-    val event = TestInt(1)
-    event.toEntity("test", "test", List()).getKey.getPath(0).getName.length shouldEqual 36
-  }
+      "work with optional values" in {
+        val event = TestOptional(None, Some("some"), None)
+        val entity = event.toEntity("test", "test", List())
+        entity.parseTo[TestOptional] shouldBe event
+      }
 
-  "Entity" should "be built with first field as key" in {
-    val event = TestInt(1)
-    event.toEntity("test", "test", List("value")).getKey.getPath(0).getName shouldEqual "1"
-  }
+    }
 
-  "Entity" should "be built with any field as key" in {
-    val event = TestStringInt("not key", 1)
-    event.toEntity("test", "test", List("i")).getKey.getPath(0).getName shouldEqual "1"
-  }
+    "created with a specific key" should {
 
-  "Entity" should "be built with any fields as composit key" in {
-    val event = TestStringIntDouble("not key", 1, 0.1)
-    event.toEntity("test", "test", List("s", "i")).getKey.getPath(0).getName shouldEqual "not key_1"
+      "have uuid key if no key field is defined" in {
+        val event = TestInt(1)
+        event.toEntity("test", "test", List()).getKey.getPath(0).getName.length shouldEqual 36
+      }
+
+      "be possible to build with the first field as key" in {
+        val event = TestInt(1)
+        event.toEntity("test", "test", List("value")).getKey.getPath(0).getName shouldEqual "1"
+      }
+
+      "be possible to build with any field as key" in {
+        val event = TestStringInt("not key", 1)
+        event.toEntity("test", "test", List("i")).getKey.getPath(0).getName shouldEqual "1"
+      }
+
+      "be possible to build with any of the fields creating a composit key" in {
+        val event = TestStringIntDouble("not key", 1, 0.1)
+        event.toEntity("test", "test", List("s", "i")).getKey.getPath(0).getName shouldEqual "not key_1"
+      }
+    }
   }
 }
